7a3499e9b8dff3a8b10b4bdbe65ceff1d7b2575e
林维 <devlevin@aliyun.com>
2020-03-12 20:33:19 +0800

    1

--- openssl@1.0.rb
+++ openssl@1.0.rb
@@ -1,4 +1,4 @@
-class OpensslAT11 < Formula
+class OpensslAT10 < Formula
   desc "Cryptography and SSL/TLS Toolkit"
   homepage "https://openssl.org/"
   url "https://www.openssl.org/source/openssl-1.0.0.tar.gz"
